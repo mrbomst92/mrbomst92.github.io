02f625ddfa32ac29390a003de4e18f990a840b3b
quách thế anh <35905515+mrbomst92@users.noreply.github.com>
2018-03-05 22:53:47 +0700

    f

--- packages.sh
+++ packages.sh
@@ -1,1 +1,9 @@
-dpkg-deb -Zgzip -b Pikabu
+#dpkg-deb -Zgzip -b Portrait
+
+dpkg-deb -b Portrait
+dpkg-deb -b MitsuhaXI
+dpkg-deb -b AdMissile
+dpkg-deb -b FilzaElectracuted
+dpkg-deb -b Pikabu
+dpkg-deb -b FluidEnabler
+dpkg-deb -b Fluidity
